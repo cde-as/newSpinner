74268714b1b93079a40572799b62f6224483b5ba
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-10 09:54:14 -0800

    removed unnecessary comments

--- spinner2.js
+++ spinner2.js
@@ -1,27 +1,3 @@
-/* setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r-/   ');
-}, 1100); */
-
 let sentence = "\r| , \r/ , \r- , \r\\ , \r| , \r-/ ";
 let eachWord = sentence.split(" ,");
 
